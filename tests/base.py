24e8f491248d3f6baaa795f53e306bee6c8f33e9
Iacopo Spalletti <i.spalletti@nephila.it>
2020-11-01 16:45:44 +0100

    Drop python 2 / Django < 2.2
    - Update tooling to github actions / pre-commit / bumpversion / towncrier

--- tests/base.py
+++ tests/base.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
-
 from app_helper.base_test import BaseTestCase
 from django.core.cache import cache
 
 
 class BaseRedirectTest(BaseTestCase):
-
     def setUp(self):
-        super(BaseRedirectTest, self).setUp()
+        super().setUp()
         cache.clear()
